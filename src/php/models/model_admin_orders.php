f29cb19a0677fb22d6d0a243bf594479bd900c6e
Vladimir Markov <markov.vov@gmail.com>
2020-04-21 15:20:30 +0700

    get data method changed to multiple joins instead of double WITH

--- src/php/models/model_admin_orders.php
+++ src/php/models/model_admin_orders.php
@@ -7,21 +7,12 @@ class model_admin_orders extends model
     $dbh = new PDO($this->dsn, $this->db_username, $this->db_password);
     $stmt = $dbh->prepare(
       "
-      WITH orders_and_users(order_id, user_id, total_price, username)
-      AS 
-      (
-        SELECT  orders.order_id, orders.user_id, orders.total_price, users.username FROM orders
-        LEFT JOIN users ON orders.user_id = users.user_id
-      ),
-      items_joined (order_id, item_id, name, price, quantity)
-	    AS
-	    (
-        SELECT orders_items.order_id, orders_items.item_id, items.name, items.price, orders_items.quantity FROM orders_items
-        LEFT JOIN items ON items.item_id = orders_items.item_id
-      )
-      SELECT * FROM orders_and_users
-      RIGHT JOIN items_joined ON orders_and_users.order_id = items_joined.order_id
-      ");
+      SELECT orders.order_id, orders.user_id, orders.total_price, users.username, orders_items.order_id, orders_items.item_id, items.name, items.price, orders_items.quantity
+      FROM orders
+      LEFT JOIN users ON orders.user_id = users.user_id
+      LEFT JOIN orders_items ON orders.order_id = orders_items.order_id
+      LEFT JOIN items ON orders_items.item_id = items.item_id"
+    );
     $stmt->execute();
     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $data;
